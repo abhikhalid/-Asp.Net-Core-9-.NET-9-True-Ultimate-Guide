44f04b762b76cfcc186584a879ec553a749b4728
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-12-29 20:17:10 +0600

    HTTP_Request

--- Section 3 -  HTTP [MVC and Web API]/HTTP/Program.cs
+++ Section 3 -  HTTP [MVC and Web API]/HTTP/Program.cs
@@ -4,12 +4,12 @@ var app = builder.Build();
 //When a browser sends a request to the Kestrel and Kestrl forwards the same request to the application code, then asp dot net core automatically creates an object of type http context
 app.Run(async (HttpContext context) =>
 {
-    context.Response.Headers["MyKey"] = "my value"; //Response.Headers is a Dictionary in C#
-    context.Response.Headers["Server"] = "My server"; //Response.Headers is a Dictionary in C#
-    context.Response.Headers["Content-Type"] = "text/html"; //Response.Headers is a Dictionary in C#
-    //this will be added to the response body
-    await context.Response.WriteAsync("<h1>Hello</h1>");
-    await context.Response.WriteAsync("<h2>World</h2>");
+    string path = context.Request.Path;
+    string method = context.Request.Method;
+
+    context.Response.Headers["Content-type"] = "text/html";
+    await context.Response.WriteAsync(path);
+    await context.Response.WriteAsync(method);
 });
 
 app.Run();
