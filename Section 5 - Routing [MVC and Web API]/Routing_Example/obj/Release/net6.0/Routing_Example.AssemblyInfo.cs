c8b18be75779aaa643eab58292453a4f46f96b99
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2025-01-01 20:18:23 +0600

    Route_Constraints_Part_2

--- Section 5 - Routing [MVC and Web API]/Routing_Example/obj/Release/net6.0/Routing_Example.AssemblyInfo.cs
+++ Section 5 - Routing [MVC and Web API]/Routing_Example/obj/Release/net6.0/Routing_Example.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Routing_Example")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7b8fbd19776615ce1d19db8a031304480e3e7fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a77c514ad907d41570f8784bda2ced687fde0ae2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Routing_Example")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Routing_Example")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
