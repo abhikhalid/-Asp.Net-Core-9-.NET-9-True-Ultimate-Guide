1902fdde376b43dd0015faa86f9ace7190199041
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2025-01-01 22:54:10 +0600

    Section_06_Controllers_and_IActionResult

---
+++ Section 06 - Controllers & IActionResult [MVC]/Controllers_Example/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Controllers_Example.Controllers
+{
+    //this class needs to be public then only it can be instantiated by ASP.NET CORE Internally.
+    public class HomeController
+    {
+        //this is called attribute routing
+        [Route("/")]
+        // this method could return anything
+        public string method1()
+        {
+            return "Hello from method1!";
+        }
+    }
+}
---
+++ Section 06 - Controllers & IActionResult [MVC]/Controllers_Example/Program.cs
@@ -0,0 +1,30 @@
+using Controllers_Example.Controllers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// In asp.dot net core, Controllers are also services. and you are required to add all the services to the application builder.
+//builder.Services.AddTransient<HomeController>(); 
+
+// but in a large application, you will have lots of controller. so let do the short-cut
+//ASP.NET Core automatically detects what are the controller present in the application
+
+builder.Services.AddControllers(); //adds all the controller classes as service
+
+var app = builder.Build();
+
+//let's enable routing
+/*
+ 
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    //it will detect all the controllers of your entire project and it will pickup all the action methods and for all the action methods, routing will be added at a time.
+    endpoints.MapControllers();
+});
+
+*/
+
+// let's simply the above process
+app.MapControllers();
+
+app.Run();
