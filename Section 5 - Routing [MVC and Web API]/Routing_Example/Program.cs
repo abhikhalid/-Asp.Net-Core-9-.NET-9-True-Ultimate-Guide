6fa58535cb901bbaf1f5c17040991174d5e2a97f
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2025-01-01 18:26:05 +0600

    Route_Parameters

--- Section 5 - Routing [MVC and Web API]/Routing_Example/Program.cs
+++ Section 5 - Routing [MVC and Web API]/Routing_Example/Program.cs
@@ -1,49 +1,31 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.Use(async (context, next) =>
-{
-    Endpoint? endpoint = context.GetEndpoint(); //null
-    if(endpoint != null)
-    {
-        await context.Response.WriteAsync($"Endpoint: {endpoint.DisplayName}]n");
-    }
-
-    await next(context);
-});
-
 //enable routing
 app.UseRouting();
-
-app.Use(async (context, next) =>
-{
-    Endpoint? endpoint = context.GetEndpoint();
-    if (endpoint != null)
-    {
-        await context.Response.WriteAsync($"Endpoint: {endpoint.DisplayName}]n");
-    }
-    await next(context);
-});
-
 
 //creating end points
 app.UseEndpoints(endpoints =>
 {
-    //add your end points
-    //this does not forward req to next middleware. so it is a short-circuiting middleware.
-    endpoints.MapGet("map1", async (context) =>
+    endpoints.Map("files/{filename}.{extension}", async (context) =>
     {
-        await context.Response.WriteAsync("In Map 1");
+        string? fileName = Convert.ToString(context.Request.RouteValues["filename"]);
+        string? extension = Convert.ToString(context.Request.RouteValues["extension"]);
+        await context.Response.WriteAsync($"In files - {fileName} - {extension}");
     });
 
-    endpoints.MapPost("map2", async (context) =>
+    //route parameter name are case insensitive
+    endpoints.Map("employee/profile/{employeeName}", async (context) =>
     {
-        await context.Response.WriteAsync("In Map 2");
+       string? employeeName =  Convert.ToString(context.Request.RouteValues["employeename"]); // by default it returns object type. We have converted into string type
+        await context.Response.WriteAsync($"In Employee profile - {employeeName}");
     });
 });
 
+app.Run(async context =>
+{
+    await context.Response.WriteAsync($"Request received at {context.Request.Path}");
+});
+
 app.Run();
-//app.Run(async context =>
-//{
-//    await context.Response.WriteAsync($"Request received at {context.Request.Path}");
-//});
+
