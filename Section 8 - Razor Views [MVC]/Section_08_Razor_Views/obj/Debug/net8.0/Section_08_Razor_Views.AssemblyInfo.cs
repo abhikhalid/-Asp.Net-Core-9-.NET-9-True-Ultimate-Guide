5cb329e68fc4f3a65491d7da5f623d9acb54012e
kmahmud <kmahmud@relisource.com>
2025-01-14 12:26:11 +0600

    Foreach

--- Section 8 - Razor Views [MVC]/Section_08_Razor_Views/obj/Debug/net8.0/Section_08_Razor_Views.AssemblyInfo.cs
+++ Section 8 - Razor Views [MVC]/Section_08_Razor_Views/obj/Debug/net8.0/Section_08_Razor_Views.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Section_08_Razor_Views")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d86dbd7b4478edeb7171a2f54ada3c8728ebc5b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d699692189b63c9a3c3d8f17e0c898914a223cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Section_08_Razor_Views")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Section_08_Razor_Views")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
