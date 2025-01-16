4582fbaa6af05c94b82fc35db6df02c78db38f87
kmahmud <kmahmud@relisource.com>
2025-01-16 13:02:45 +0600

    Nested Layout Views  + NOTE

--- Section 9 - Layout Views/LayoutViewsExample/obj/Debug/net8.0/LayoutViewsExample.AssemblyInfo.cs
+++ Section 9 - Layout Views/LayoutViewsExample/obj/Debug/net8.0/LayoutViewsExample.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LayoutViewsExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+934ccaa00e2f14aedd6b78a8a0655081fa28d2ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+668a75ba505fb66d844e22afacaea5326bc10d18")]
 [assembly: System.Reflection.AssemblyProductAttribute("LayoutViewsExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LayoutViewsExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
