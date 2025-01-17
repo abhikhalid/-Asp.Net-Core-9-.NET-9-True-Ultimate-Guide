3365307f44a12acab50b2af2a4950cf48ae7d8ac
kmahmud <kmahmud@relisource.com>
2025-01-17 23:46:05 +0600

    Services - Part 2

---
+++ Section 12 - Dependency Injection - MVC and Web API/DIExample/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace DIExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public readonly CitiesService _citiesService;
+
+        public HomeController()
+        {
+            //create object of CitiesService class
+            _citiesService = new CitiesService();
+        }
+
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            //we are in the controller, without worrying about where actual citiesService are coming, we can simply demand the service.
+            //now, it is responsibility of the service to provide the data.
+            List<string> cities = _citiesService.GetCities();
+            //now we can supply the cities to the view so that the view can be strongly typed view.
+            return View(cities);
+        }
+    }
+}
--- Section 12 - Dependency Injection - MVC and Web API/DIExample/obj/Debug/net8.0/DIExample.AssemblyInfo.cs
+++ Section 12 - Dependency Injection - MVC and Web API/DIExample/obj/Debug/net8.0/DIExample.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DIExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e8e386133486e1129b7ebe23e7937ad8ec50ee0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a19f0d0291cb4287e9b16cb5d4f86b04b4a1054")]
 [assembly: System.Reflection.AssemblyProductAttribute("DIExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DIExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
