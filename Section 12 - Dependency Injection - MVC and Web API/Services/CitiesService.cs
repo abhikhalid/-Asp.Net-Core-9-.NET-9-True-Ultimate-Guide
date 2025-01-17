1a19f0d0291cb4287e9b16cb5d4f86b04b4a1054
kmahmud <kmahmud@relisource.com>
2025-01-17 22:56:59 +0600

    Section 12: Dependency Injection [MVC and Web API]

---
+++ Section 12 - Dependency Injection - MVC and Web API/Services/CitiesService.cs
@@ -0,0 +1,24 @@
+﻿namespace Services
+{
+    public class CitiesService
+    {
+        private List<string> _cities;
+
+        public CitiesService()
+        {
+            _cities = new List<string>()
+            {
+                "New York",
+                "London",
+                "Paris",
+                "Berlin",
+                "Tokyo"
+            };
+        }
+
+        public List<string> GetCities()
+        {
+            return _cities;
+        }
+    }
+}
