9e97636784b707ae8a407bd7eee360191b9cfae9
kmahmud <kmahmud@relisource.com>
2025-01-17 16:37:57 +0600

    Creeating View Components

--- Section 11 - View Components [MVC]/ViewComponents_Example/ViewComponents/GridViewComponent.cs
+++ Section 11 - View Components [MVC]/ViewComponents_Example/ViewComponents/GridViewComponent.cs
@@ -5,11 +5,15 @@ namespace ViewComponents_Example.ViewComponents
     [ViewComponent] //this attribute is optional as long as the class name ends with "ViewComponent"
     public class GridViewComponent : ViewComponent
     {
-        Task<IViewComponentResult> InvokeAsync()
+        public async Task<IViewComponentResult> InvokeAsync()
         {
+            //here we can write the logic to get data from database or any other source or calculation logic
+
+
             //invoked a partial view
             //the default location of partial view is Views/Shared/Components/Grid/Default.cshtml
             return View(); 
+            //return View("Sample"); //do this if you change the partial view name instead of 'Default.cshtml' 
         }
     }
 }
