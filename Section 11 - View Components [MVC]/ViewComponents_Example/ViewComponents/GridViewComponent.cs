072e42b35df8905fcd6ce27349c01af8210a4e61
kmahmud <kmahmud@relisource.com>
2025-01-17 16:12:24 +0600

    Section 11: View Components

---
+++ Section 11 - View Components [MVC]/ViewComponents_Example/ViewComponents/GridViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewComponents_Example.ViewComponents
+{
+    [ViewComponent] //this attribute is optional as long as the class name ends with "ViewComponent"
+    public class GridViewComponent : ViewComponent
+    {
+        Task<IViewComponentResult> InvokeAsync()
+        {
+            //invoked a partial view
+            //the default location of partial view is Views/Shared/Components/Grid/Default.cshtml
+            return View(); 
+        }
+    }
+}
