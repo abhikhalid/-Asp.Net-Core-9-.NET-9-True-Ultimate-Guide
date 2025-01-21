57029d6f801f48aedf816eedb9f1f8cabddc917f
kmahmud <kmahmud@relisource.com>
2025-01-21 19:25:27 +0600

    IConfiguration in Controller

---
+++ Section 14 - Configuration [MVC and Web API]/ConfigurationExample/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConfigurationExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private field
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewBag.Mykey = _configuration["MyKey"];
+            ViewBag.MyAPIkey = _configuration.GetValue("MyAPIKey", "the default key"); //added fallback value if the key is missing
+
+            return View();
+        }
+    }
+}
--- Section 14 - Configuration [MVC and Web API]/ConfigurationExample/obj/Debug/net8.0/ConfigurationExample.AssemblyInfo.cs
+++ Section 14 - Configuration [MVC and Web API]/ConfigurationExample/obj/Debug/net8.0/ConfigurationExample.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConfigurationExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f14af165dcc03edb2c82ed2192e164a0075d9b38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d463cbf76fd0461daa823d18c388a7dff540ece")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConfigurationExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConfigurationExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
