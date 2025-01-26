7506252178fc1dd5ea61e9350a82647fe941d82d
kmahmud <kmahmud@relisource.com>
2025-01-26 17:42:18 +0600

    Get All Countries - xUnit Test Get All Countries - Implementation

--- Section 15 - xUnit [MVC and Web API]/CRUDExample/obj/Debug/net8.0/CRUDExample.AssemblyInfo.cs
+++ Section 15 - xUnit [MVC and Web API]/CRUDExample/obj/Debug/net8.0/CRUDExample.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUDExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a5edbba1d0c5dea66f6af6e3b1542140d9e9eb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+535ab25f6d3509b6b35da81166145c5e20476912")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUDExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUDExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Section 15 - xUnit [MVC and Web API]/ServiceContracts/DTO/CountryResponse.cs
+++ Section 15 - xUnit [MVC and Web API]/ServiceContracts/DTO/CountryResponse.cs
@@ -11,6 +11,24 @@ namespace ServiceContracts.DTO
         public Guid CountryID { get; set; }
 
         public string? CountryName { get; set; }
+
+        //It compares the current object to another object of CountryResponse type and return true if both values are same, otherwise return false
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+
+            if (obj.GetType() != typeof(CountryResponse)) return false;
+
+            CountryResponse country_to_compare = (CountryResponse)obj;
+
+            return this.CountryID == country_to_compare.CountryID && this.CountryName == country_to_compare.CountryName;
+        }
+
+        //when you override, Equals() method, it is advisible to override 'GetHashCode()' method.
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 
     //assume that, we are adding this method to 'Country Class'
