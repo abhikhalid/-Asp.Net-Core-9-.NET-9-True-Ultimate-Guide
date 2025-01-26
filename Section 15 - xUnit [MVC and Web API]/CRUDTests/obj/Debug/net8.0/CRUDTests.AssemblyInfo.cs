3e8645482a31557e7b9f43870c3067a4888fe769
kmahmud <kmahmud@relisource.com>
2025-01-27 00:01:20 +0600

    Get All Persons - Implementation

--- Section 15 - xUnit [MVC and Web API]/CRUDTests/obj/Debug/net8.0/CRUDTests.AssemblyInfo.cs
+++ Section 15 - xUnit [MVC and Web API]/CRUDTests/obj/Debug/net8.0/CRUDTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUDTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e7664c49f3c0dd3d482e94516eb7dbd41720010")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb12de85edfc2680ace259e665ecafee58fbcaa3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUDTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUDTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
