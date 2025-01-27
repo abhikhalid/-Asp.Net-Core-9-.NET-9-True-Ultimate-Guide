e21a0a2e99cbe8531d2e2b548b5599edf5b5092a
kmahmud <kmahmud@relisource.com>
2025-01-27 23:33:50 +0600

    Get Sorted Persons - Implementation

--- Section 15 - xUnit [MVC and Web API]/CRUDExample/obj/Debug/net8.0/CRUDExample.AssemblyInfo.cs
+++ Section 15 - xUnit [MVC and Web API]/CRUDExample/obj/Debug/net8.0/CRUDExample.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUDExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4a365302d418a6dfc78e1ebb2a63ed7f4df6c5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17d96cc95e9c01069ad40fd0c36b065f7fa7ed5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUDExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUDExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
