d5323b149f807b54595cf09a73c48afe7a5cb721
kmahmud <kmahmud@relisource.com>
2025-02-05 11:39:48 +0600

    Seed Data

--- Section 19 - EntityFrameworkCore [MVC and Web API]/Entities/PersonsDbContext.cs
+++ Section 19 - EntityFrameworkCore [MVC and Web API]/Entities/PersonsDbContext.cs
@@ -20,6 +20,32 @@ namespace Entities
 
             modelBuilder.Entity<Country>().ToTable("Countries"); //I want the table name to be 'Countries'. you can define any other name.
             modelBuilder.Entity<Person>().ToTable("Persons"); 
+
+            //To add the seed data we have to use this on model creating method in the dbcontext class.
+            //so after you map your model class to the table. here is the place for that.
+
+            //Seed to Countries
+            //modelBuilder.Entity<Country>().HasData(new Country() { CountryID = Guid.NewGuid(), CountryName = "Bangladesh"});
+
+            //but there is a more shortcut way that this.
+
+            string countriesJson = System.IO.File.ReadAllText("countries.json");
+            List<Country> countries = System.Text.Json.JsonSerializer.Deserialize<List<Country>>(countriesJson);
+
+            foreach (Country country in countries)
+            {
+                modelBuilder.Entity<Country>().HasData(country);
+            } 
+            
+            //Seed Persons
+            string personsJson = System.IO.File.ReadAllText("persons.json");
+            List<Person> persons = System.Text.Json.JsonSerializer.Deserialize<List<Person>>(personsJson);
+
+            foreach (Person pesron in persons)
+            {
+                modelBuilder.Entity<Person>().HasData(pesron);
+            }
+
         }
     }
 }
--- Section 19 - EntityFrameworkCore [MVC and Web API]/ServiceContracts/obj/Debug/net8.0/ServiceContracts.AssemblyInfo.cs
+++ Section 19 - EntityFrameworkCore [MVC and Web API]/ServiceContracts/obj/Debug/net8.0/ServiceContracts.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceContracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d221f08d8fd41e2359f236c57144cee144ae3402")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8470b674d746702273b391a1c7471aff0f30d271")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceContracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceContracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
