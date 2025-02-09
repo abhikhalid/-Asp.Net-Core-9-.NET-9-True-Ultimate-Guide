5d3096776259a378be81f886166ddf2d1c629f4c
kmahmud <kmahmud@relisource.com>
2025-02-09 15:51:38 +0600

    Excel to Database Upload - Part 2

---
+++ Section 19 - EntityFrameworkCore [MVC and Web API]/CRUDExample/Controllers/CountriesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDExample.Controllers
+{
+    [Controller]
+    public class CountriesController : Controller
+    {
+        [Route("UploadFromExcel")]
+        public IActionResult UploadFromExcel()
+        {
+            return View();
+        }
+    }
+}
--- Section 19 - EntityFrameworkCore [MVC and Web API]/CRUDExample/obj/Debug/net8.0/CRUDExample.AssemblyInfo.cs
+++ Section 19 - EntityFrameworkCore [MVC and Web API]/CRUDExample/obj/Debug/net8.0/CRUDExample.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUDExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24507038124bb9f06a5a983a3e4775141fb63449")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c514c73861cda56ce0f33ce53b45bcceafa049d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUDExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUDExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
