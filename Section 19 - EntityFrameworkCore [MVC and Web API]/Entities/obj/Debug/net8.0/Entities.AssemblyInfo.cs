c13a0e10dd2b411df02e1e727c260b9ffea1a29d
kmahmud <kmahmud@relisource.com>
2025-02-09 12:30:26 +0600

    added package

--- Section 19 - EntityFrameworkCore [MVC and Web API]/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ Section 19 - EntityFrameworkCore [MVC and Web API]/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65fd2638bd75c17d9510318f286c7ea3e4be9e2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b275910795cf7a95fb32f0ac3bf587664845cbc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
