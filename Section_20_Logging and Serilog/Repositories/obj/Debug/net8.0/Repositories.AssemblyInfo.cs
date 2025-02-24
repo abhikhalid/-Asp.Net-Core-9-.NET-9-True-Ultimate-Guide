9cb155cba72a1d7b242eac5b2567181fbdbe4fbb
kmahmud <kmahmud@relisource.com>
2025-02-24 14:05:25 +0600

    Serilog Database Sink

--- Section_20_Logging and Serilog/Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
+++ Section_20_Logging and Serilog/Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17e01a4c8e616aebcc1ff5e5e86aa4fad9a3c087")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d932bcc5b23c72c61a4e32f9d36d5afc4ca64107")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
