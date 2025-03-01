4bc9d680fab89e89aeae4a8cfc7b2c7396d68ee1
kmahmud <kmahmud@relisource.com>
2025-03-01 18:47:23 +0600

    Section 27: Swagger/Open API [Web API]

---
+++ Section_27_Swagger_Open_API_Web_API/CitiesManager.WebAPI/Controllers/CitiesController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CitiesManager.WebAPI.DatabaseContext;
+using CitiesManager.WebAPI.Models;
+
+namespace CitiesManager.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] //The ApiController attribute makes the class as a controller class.
+    public class CitiesController : ControllerBase //The Controller class must be suffix with controller and inherit from ControllerBase, if it is not then it will not be considered as a controller.
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CitiesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Cities
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<City>>> GetCities()
+        {
+            return await _context.Cities.ToListAsync();
+        }
+
+        // GET: api/Cities/5
+        [HttpGet("{cityID}")]
+        public async Task<ActionResult<City>> GetCity(Guid cityID)
+        {
+            var city = await _context.Cities.FirstOrDefaultAsync(temp => temp.CityID == cityID);
+
+            if (city == null)
+            {
+                return Problem(detail:"Invalid CityID", statusCode: 404, title:"City Search"); //HTTP 404 Not Found
+                //return BadRequest();
+            }
+
+            return city;
+        }
+
+        // PUT: api/Cities/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{cityId}")]
+        public async Task<IActionResult> PutCity(Guid cityId, City city)
+        {
+            if (cityId != city.CityID)
+            {
+                return BadRequest(); //HTTP 400 Bad Request
+            }
+
+            var existingCity = await _context.Cities.FindAsync(cityId);
+            if(existingCity == null)
+            {
+                return NotFound(); //HTTP 404 Not Found
+            }
+
+            existingCity.CityName = city.CityName;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CityExists(cityId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Cities
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<City>> PostCity(City city)
+        {
+            //this is automatically done by the ApiController attribute
+            //if(ModelState.IsValid == false)
+            //{
+            //    return ValidationProblem(ModelState);
+            //}
+
+            if (_context.Cities == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Cities' is null.");
+            }
+
+            _context.Cities.Add(city);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCity", new { id = city.CityID }, city);
+        }
+
+        // DELETE: api/Cities/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCity(Guid id)
+        {
+            var city = await _context.Cities.FindAsync(id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            _context.Cities.Remove(city);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CityExists(Guid id)
+        {
+            return _context.Cities.Any(e => e.CityID == id);
+        }
+    }
+}
