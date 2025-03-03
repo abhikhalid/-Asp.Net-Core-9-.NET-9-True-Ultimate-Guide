fc147857571601cae0d986db8070f3cb3cacbe5b
kmahmud <kmahmud@relisource.com>
2025-03-03 16:13:10 +0600

    Section 21: Filters [MVC and Web API] - Project Setup

---
+++ Section_21_Filters/CRUDExample/Program.cs
@@ -0,0 +1,100 @@
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Repositories;
+using RepositoryContracts;
+using ServiceContracts;
+using Services;
+using Serilog;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Logging
+//builder.Host.ConfigureLogging(loggingProvider =>
+//{
+//    loggingProvider.ClearProviders();
+//    loggingProvider.AddConsole();
+//    //loggingProvider.AddDebug();
+//    //loggingProvider.AddEventLog();
+//});
+
+//replace existing logging mechanism with serilog
+builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider services,
+   LoggerConfiguration loggerConfiguration) =>
+ {
+     loggerConfiguration
+     .ReadFrom.Configuration(context.Configuration) // means, reading the configuration from appSettings.json
+     .ReadFrom.Services(services); // this statemenet makes our service collection available to Siri log. As a part of that, any serilog sync can access the services of our application.
+ });
+
+
+builder.Services.AddControllersWithViews();
+
+//add services into IoC container
+builder.Services.AddScoped<ICountriesGetterService, CountriesGetterService>();
+builder.Services.AddScoped<ICountriesAdderService, CountriesAdderService>();
+builder.Services.AddScoped<ICountriesUploaderService,CountriesUploaderService>();
+
+//builder.Services.AddScoped<IPersonsGetterService,PersonsGetterServiceWithFewExcelFields>();
+builder.Services.AddScoped<IPersonsGetterService,PersonsGetterServiceChild>();
+builder.Services.AddScoped<PersonsGetterService, PersonsGetterService>();
+
+
+builder.Services.AddScoped<IPersonsAdderService, PersonsAdderService>();
+builder.Services.AddScoped<IPersonsDeleterService, PersonsDeleterService>();
+builder.Services.AddScoped<IPersonsSorterService, PersonsSorterService>();
+builder.Services.AddScoped<IPersonsUpdaterService, PersonsUpdaterService>();
+
+builder.Services.AddScoped<IPersonsRepository, PersonsRepository>();
+builder.Services.AddScoped<ICountriesRepository, CountriesRepository>();
+
+builder.Services.AddHttpLogging(options =>
+{
+    options.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.RequestProperties | Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.ResponsePropertiesAndHeaders;
+});
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    //means, hey asp dot net core, we are trying to use asp dot net core for database connection
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+//Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=PersonsDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False
+
+//Integrated Security=True (means, we are using windows authentication for sql server. that means you do not need separate user id and password)
+
+var app = builder.Build();
+
+//it enables the endpoint completion log, means it adds an extra log message as soon as the requsest resposne is completed
+app.UseSerilogRequestLogging(); 
+
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+
+//app.Logger.LogDebug("debug-message");
+//app.Logger.LogInformation("information-message");
+//app.Logger.LogWarning("warning-message");
+//app.Logger.LogError("error-message");
+//app.Logger.LogCritical("critical-message");
+
+if (!builder.Environment.IsEnvironment("Test"))
+{
+    Rotativa.AspNetCore.RotativaConfiguration.Setup("wwwroot", wkhtmltopdfRelativePath: "Rotativa");
+}
+
+app.UseHttpLogging();
+app.Run();
+
+public partial class Program  //make the auto-generated Program accessible programmatically
+{
+
+}
---
+++ Section_21_Filters/CRUDTests/PersonsControllerTest.cs
@@ -0,0 +1,135 @@
+﻿using AutoFixture;
+using CRUDExample.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Serilog;
+using ServiceContracts;
+using ServiceContracts.DTO;
+using ServiceContracts.Enums;
+using Services;
+using System;
+using System.Collections.Generic;
+
+namespace CRUDTests
+{
+    public class PersonsControllerTest
+    {
+        private readonly IPersonsGetterService _personsGetterService;
+        private readonly IPersonsAdderService _personsAdderService;
+        private readonly IPersonsDeleterService _personsDeleterService;
+        private readonly IPersonsSorterService _personsSorterService;
+        private readonly IPersonsUpdaterService _personsUpdaterService;
+
+        private readonly ICountriesGetterService _countriesGetterService;
+
+        private readonly Mock<ICountriesGetterService> _countriesGetterServiceMock;
+        private readonly Mock<IPersonsGetterService> _personGetterServiceMock;
+        private readonly Mock<IPersonsAdderService> _personAdderServiceMock;
+        private readonly Mock<IPersonsDeleterService> _personDeleterServiceMock;
+        private readonly Mock<IPersonsSorterService> _personSorterServiceMock;
+        private readonly Mock<IPersonsUpdaterService> _personUpdaterServiceMock;
+
+        private readonly ILogger<PersonsController> _logger;
+        private readonly Mock<ILogger<PersonsController>> _loggerMock;
+
+
+        private readonly Fixture _fixture;
+
+        public PersonsControllerTest()
+        {
+            _fixture = new Fixture();
+            _countriesGetterServiceMock = new Mock<ICountriesGetterService>();
+
+            _personGetterServiceMock = new Mock<IPersonsGetterService>();
+            _personAdderServiceMock = new Mock<IPersonsAdderService>();
+            _personDeleterServiceMock = new Mock<IPersonsDeleterService>();
+            _personSorterServiceMock = new Mock<IPersonsSorterService>();
+            _personUpdaterServiceMock = new Mock<IPersonsUpdaterService>();
+
+            _countriesGetterService = _countriesGetterServiceMock.Object;
+            _personsGetterService = _personGetterServiceMock.Object;
+            _personsAdderService = _personAdderServiceMock.Object;
+            _personsDeleterService = _personDeleterServiceMock.Object;
+            _personsSorterService = _personSorterServiceMock.Object;
+            _personsUpdaterService = _personUpdaterServiceMock.Object;
+
+            _loggerMock = new Mock<ILogger<PersonsController>>();
+            _logger = _loggerMock.Object;
+        }
+
+        #region Index
+        [Fact]
+        public async Task Index_ShouldReturnIndexViewWithPersonsList()
+        {
+            //Arrange
+            List<PersonResponse> persons_response_list = _fixture.Create<List<PersonResponse>>();
+
+            PersonsController personsController = new PersonsController(_personsGetterService,_personsAdderService,_personsSorterService, _personsUpdaterService, _personsDeleterService, _countriesGetterService, _logger);
+
+            _personGetterServiceMock.Setup(temp => temp.GetFilteredPersons(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(persons_response_list);
+            _personSorterServiceMock.Setup(temp => temp.GetSortedPersons(It.IsAny<List<PersonResponse>>(), It.IsAny<string>(), It.IsAny<SortOrderOptions>())).ReturnsAsync(persons_response_list);
+
+            //Act
+            IActionResult result = await personsController.Index(_fixture.Create<string>(),_fixture.Create<string>(), _fixture.Create<string>(), _fixture.Create<SortOrderOptions>());
+
+            //Assert
+            ViewResult viewResult = Assert.IsType<ViewResult>(result);
+            viewResult.ViewData.Model.Should().BeAssignableTo<List<PersonResponse>>();
+            viewResult.ViewData.Model.Should().Be(persons_response_list);
+        }
+        #endregion
+
+        #region Create
+        [Fact]
+        public async void Create_IfModelErrors_ToReturnCreateView()
+        {
+            //Arrange
+            PersonAddRequest person_add_request = _fixture.Create<PersonAddRequest>();
+
+            PersonResponse person_response = _fixture.Create<PersonResponse>();
+            List<CountryResponse> countries = _fixture.Create<List<CountryResponse>>();
+
+            _countriesGetterServiceMock.Setup(temp => temp.GetAllCountries()).ReturnsAsync(countries);
+            _personAdderServiceMock.Setup(temp => temp.AddPerson(It.IsAny<PersonAddRequest>())).ReturnsAsync(person_response);
+
+            PersonsController personsController = new PersonsController(_personsGetterService, _personsAdderService, _personsSorterService, _personsUpdaterService, _personsDeleterService, _countriesGetterService, _logger);
+
+
+            //Act
+            personsController.ModelState.AddModelError("PersonName", "Person Name can't be blank");
+
+            IActionResult result = await personsController.Create(person_add_request);
+
+            //Assert
+            ViewResult viewResult = Assert.IsType<ViewResult>(result);
+            viewResult.ViewData.Model.Should().BeAssignableTo<PersonAddRequest>();
+            viewResult.ViewData.Model.Should().Be(person_add_request);
+        }
+
+        [Fact]
+        public async void Create_IfNoModelErrors_ToReturnRedirectToIndex()
+        {
+            //Arrange
+            PersonAddRequest person_add_request = _fixture.Create<PersonAddRequest>();
+
+            PersonResponse person_response = _fixture.Create<PersonResponse>();
+            List<CountryResponse> countries = _fixture.Create<List<CountryResponse>>();
+
+            _countriesGetterServiceMock.Setup(temp => temp.GetAllCountries()).ReturnsAsync(countries);
+            _personAdderServiceMock.Setup(temp => temp.AddPerson(It.IsAny<PersonAddRequest>())).ReturnsAsync(person_response);
+
+            PersonsController personsController = new PersonsController(_personsGetterService, _personsAdderService, _personsSorterService, _personsUpdaterService, _personsDeleterService, _countriesGetterService, _logger);
+
+
+            //Act
+            IActionResult result = await personsController.Create(person_add_request);
+
+            //Assert
+            RedirectToActionResult redirectResult = Assert.IsType<RedirectToActionResult>(result);
+            redirectResult.ActionName.Should().Be("Index");
+        }
+        #endregion
+    }
+}
