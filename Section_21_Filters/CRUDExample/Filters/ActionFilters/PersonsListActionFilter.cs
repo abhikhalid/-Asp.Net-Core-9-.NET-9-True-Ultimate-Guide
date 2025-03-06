fc05a4bc6ad9e470380f12fb0815b4cf6486765f
kmahmud <kmahmud@relisource.com>
2025-03-06 15:16:45 +0600

    Parameter Validation in Action Filter

--- Section_21_Filters/CRUDExample/Filters/ActionFilters/PersonsListActionFilter.cs
+++ Section_21_Filters/CRUDExample/Filters/ActionFilters/PersonsListActionFilter.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Filters;
+using ServiceContracts.DTO;
 
 namespace CRUDExample.Filters.ActionFilters
 {
@@ -22,6 +23,32 @@ namespace CRUDExample.Filters.ActionFilters
         public void OnActionExecuting(ActionExecutingContext context)
         {
             _logger.LogInformation("PersonsListActionFilter.OnActionExecuting method");
+
+            if(context.ActionArguments.ContainsKey("searchBy"))
+            {
+                string? searchBy = Convert.ToString(context.ActionArguments["searchBy"]);
+
+                //validate the searchBy parameter value
+                if (!string.IsNullOrEmpty(searchBy))
+                {
+                    var searchByOptions = new List<string>() {
+                        nameof(PersonResponse.PersonName),
+                        nameof(PersonResponse.Email),
+                        nameof(PersonResponse.DateOfBirth),
+                        nameof(PersonResponse.Gender),
+                        nameof(PersonResponse.CountryID),
+                        nameof(PersonResponse.Address),
+                    };
+
+                    //reset the searchBy parameter value.
+                    if(searchByOptions.Any(temp => temp == searchBy) == false)
+                    {
+                        _logger.LogInformation("SearchBy actual value {searchBy}",searchBy);
+                        context.ActionArguments["searchBy"] = nameof(PersonResponse.PersonName);
+                        _logger.LogInformation("SearchBy updated value {searchBy}", context.ActionArguments["searchBy"]);
+                    }
+                }
+            }
         }
     }
 }
--- Section_21_Filters/RepositoryContracts/obj/Debug/net8.0/RepositoryContracts.AssemblyInfo.cs
+++ Section_21_Filters/RepositoryContracts/obj/Debug/net8.0/RepositoryContracts.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepositoryContracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc147857571601cae0d986db8070f3cb3cacbe5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4908b59cfcd1363124c8ab226b0108588ac5538")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepositoryContracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepositoryContracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
